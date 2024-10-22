e95067e8835f2ac1732ddf1ff4022d39e4729aea
FridaWikman <frida.wikman@hotmail.com>
2024-10-22 15:20:36 +0200

    added !

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express(),
   port = process.env.PORT || 3000
 
 app.get('/', (_request, response) => {
-  response.send({ hello: 'World' })
+  response.send({ hello: 'World!' })
 })
 
 app.listen(port, () => {
